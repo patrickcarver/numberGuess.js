e639b2541b78f7374c04e3a7d2fdf69907d53cfa
Patrick Carver <patrickcarver@gmail.com>
2017-08-12 20:46:02 -0500

    create random num func; create model

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var model = {
+  answer: getRandomInt(1, 100),
+  numberOfGuesses: 10
+};
+
 var content = document.getElementById('content');
 var button = document.getElementById('submit-button');
 var checkGuessEvent = new Event('check guess');
 
-content.addEventListener('check guess', function() { 
-  console.log("check guess");
-});
+function checkGuess() {
+  
+}
 
-button.addEventListener('click', function() { 
-  content.dispatchEvent(checkGuessEvent);  
-});
+function onSubmit() {
+  content.dispatchEvent(checkGuessEvent); 
+}
+
+content.addEventListener('check guess', checkGuess);
+
+button.addEventListener('click', onSubmit);
